3aac0585a14b8deab829e8f93ce8a54487933bda
Nikita Kharlap <harlap19@gmail.com>
2022-03-19 20:50:15 +0300

    add reviews block

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
-import * as functions from './modules/functions.js';
-
-console.log('Hello world!');
-
-functions.func();
+// import * as functions from './modules/functions.js';
 
 
+//####################################################################################3
+// Value Buttons Block
 var valueBtns = document.querySelectorAll(".btn-value");
 var capacityImg = document.getElementById('capacity__img');
 var chosedValue = 0;
@@ -30,4 +28,36 @@ valueBtns.forEach((btn, index) => {
 var orderGasBtn = document.querySelector(".capacity__btn");
 orderGasBtn.addEventListener('click', function () {
     console.log("You orders car with", chosedValue, 'gas capacity');
+});
+
+
+
+//####################################################################################3
+// Reviews Block
+
+var arrowBack = document.querySelector(".reviews__arrow-back");
+var arrowForward = document.querySelector(".reviews__arrow-forward");
+var paginationItems = document.querySelectorAll(".pagination__item");
+var selectedReviewIndex = 0;
+var cards = document.querySelectorAll(".card");
+
+paginationItems[selectedReviewIndex].classList.add('active');
+cards[selectedReviewIndex].classList.add('visible');
+
+arrowBack.addEventListener('click', function () {
+    paginationItems[selectedReviewIndex].classList.remove('active');
+    cards[selectedReviewIndex].classList.remove('visible');
+    selectedReviewIndex--;
+    if (selectedReviewIndex == -1) selectedReviewIndex = paginationItems.length - 1;
+    paginationItems[selectedReviewIndex].classList.add('active');
+    cards[selectedReviewIndex].classList.add('visible');
+});
+
+arrowForward.addEventListener('click', function () {
+    paginationItems[selectedReviewIndex].classList.remove('active');
+    cards[selectedReviewIndex].classList.remove('visible');
+    selectedReviewIndex = selectedReviewIndex + 1;
+    if (selectedReviewIndex == paginationItems.length) selectedReviewIndex = 0;
+    paginationItems[selectedReviewIndex].classList.add('active');
+    cards[selectedReviewIndex].classList.add('visible');
 });
